ceed1cdf29701a7b4a345867f11f2fc4a55fce2d
Stanislav <nullpointerexception1@gmail.com>
2011-11-26 20:48:10 +0200

    hey

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Satan::Application.config.secret_token = 'c512c3eb3a9074ddd8b499d8d9be63ab1f44718186f5bf04f242df7cca8a7a12f05d55b923306ab680dbb1999de8a8171e444f23fa32933e72d463fa3f2d3d54'
